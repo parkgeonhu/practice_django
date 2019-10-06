002cd8c1d3f50d3c07ea58fa795129b7419d4e62
parkgeonhu <parkgeonhu@gmail.com>
2019-10-06 08:16:39 +0000

    realizing social auth login

--- practice/settings.py
+++ practice/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'blog',
-    'accounts',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',  
--- practice/urls.py
+++ practice/urls.py
@@ -19,7 +19,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('^blog/', include('blog.urls')),
-    url(r'^accounts/', include('accounts.urls')),
+    path('blog/', include('blog.urls')),
     url(r'^accounts/', include('allauth.urls')),
 ]
